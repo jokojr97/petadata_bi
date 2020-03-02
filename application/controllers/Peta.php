212de30c1fe574e8e409cde61c30b08d101ff189
jokojr97 <joko.riyadi97@gmail.com>
2020-03-02 15:17:57 +0700

    update sek

---
+++ application/controllers/Peta.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Peta extends CI_Controller {
+	public function __construct()
+    {
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
+	public function index()
+	{
+		$this->load->view('peta');
+	}
+}
